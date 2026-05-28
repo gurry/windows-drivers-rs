f8bfda77cb49441385e1df772322d5d10c7e1bdb
Gurinder Singh <gurisingh@microsoft.com>
2026-05-28 11:24:19 +0530

    Avoiding panicking allocatinos: removed a println!()

--- crates/wdf-macros/src/lib.rs
+++ crates/wdf-macros/src/lib.rs
@@ -228,7 +228,6 @@ fn object_context_impl(
 
         #[allow(non_snake_case)]
         extern "C" fn #cleanup_callback_name(fw_obj: #wdf_crate_path::WDFOBJECT) {
-            #wdf_crate_path::println!("Cleanup callback called for {} addr {:#x}", stringify!(#struct_name), fw_obj as usize);
             unsafe {
                 #wdf_crate_path::drop_context::<#struct_name>(fw_obj);
             }
